3f045a949b3405ea957c61346f9cf0c00bd2198f
gouarin <loic.gouarin@gmail.com>
2021-03-17 20:21:21 +0100

    add circular progress

--- pylbm_ui/tab_widgets/parametric_widget.py
+++ pylbm_ui/tab_widgets/parametric_widget.py
@@ -9,6 +9,7 @@ import tempfile
 import shutil
 import asyncio
 import pathos
+import pathos.pools as pp
 
 from .design_space import Design_widget
 from .simulation_widget import simulation
@@ -34,18 +35,15 @@ def run_simulation(args):
 
     return True
 
-def run_parametric_study(args):
-    from pathos.multiprocessing import ProcessingPool
-    pool = ProcessingPool(nodes=4)
-    return pool.map(run_simulation, args)
-
 class parametric_widget:
     def __init__(self, test_case_widget, lb_scheme_widget):
         with out:
-            space_step = v.TextField(v_model=0.01, type='number')
-            codegen = v.Select(items=['auto', 'numpy', 'cython'], v_model='auto')
+            space_step = v.TextField(label='Space step', v_model=0.01, type='number')
+            codegen = v.Select(label='Code generator', items=['auto', 'numpy', 'cython'], v_model='auto')
 
             design = Design_widget(test_case_widget, lb_scheme_widget)
+            run = v.Btn(v_model=True, children=['Run parametric study'], class_="ma-5", color='success')
+            plotly_plot = v.Container(align_content_center=True)
 
             left_panel = [
                 space_step,
@@ -61,16 +59,14 @@ class parametric_widget:
                     v.ExpansionPanel(children=[
                         v.ExpansionPanelHeader(children=['Methods']),
                     ]),
-                ], multiple=True)
+                ], multiple=True),
+                v.Row(children=[run], align='center', justify='center'),
             ]
             #
             # Right panel
             #
-            run = v.Btn(v_model=True, children=['Run parametric study'], class_="ma-2", color='success')
-            plotly_plot = v.Layout(d_flex=True)
 
             right_panel = [
-                v.Row(children=[run]),
                 v.Row(children=[plotly_plot])
             ]
 
@@ -85,82 +81,122 @@ class parametric_widget:
                     if run.v_model:
                         print(tmp_dir.name)
 
-                        run.v_model = False
-                        run.children = ['Stop parametric study']
-                        run.color = 'error'
-
-                        test_case = copy.deepcopy(test_case_widget.get_case())
-                        lb_scheme = copy.deepcopy(lb_scheme_widget.get_case())
-
                         design_space = design.design_space()
-                        sampling = np.asarray(Lhs().generate(list(design_space.values()), 10))
+                        if design_space:
+                            run.v_model = False
+                            run.children = ['Stop parametric study']
+                            run.color = 'error'
 
-                        output = np.zeros(sampling.shape[0])
+                            test_case = copy.deepcopy(test_case_widget.get_case())
+                            lb_scheme = copy.deepcopy(lb_scheme_widget.get_case())
 
-                        simu_cfg = test_case.get_dictionary()
-                        param = simu_cfg['parameters']
-                        simu_cfg.update(lb_scheme.get_dictionary())
-                        param.update(simu_cfg['parameters'])
-                        simu_cfg['parameters'] = param
-                        if codegen.v_model != 'auto':
-                            simu_cfg['generator'] = codegen.v_model
-                        simu_cfg['codegen_option'] = {'directory': tmp_dir.name}
-                        simu_cfg['space_step'] = float(space_step.v_model)
+                            alert = v.Alert(children=['Initialize...'], class_='primary--text')
+                            plotly_plot.children = [
+                                v.Row(children=[
+                                    v.ProgressCircular(indeterminate=True, color='primary', size=70, width=4)
+                                    ], align='center', justify='center'),
+                                v.Row(children=[
+                                    alert
+                                    ], align='center', justify='center')
+                            ]
+                            sampling = np.asarray(Lhs().generate(list(design_space.values()), 10))
 
-                        bound_cfg = {}
-                        bound_tc = test_case.get_boundary()
-                        bound_sc = lb_scheme.get_boundary()
-                        for key, val in bound_tc.items():
-                            bound_cfg[key] = bound_sc[val]
+                            output = np.zeros(sampling.shape[0])
 
-                        simu_cfg.pop('dim')
-                        simu_cfg['boundary_conditions'] = bound_cfg
+                            simu_cfg = test_case.get_dictionary()
+                            param = simu_cfg['parameters']
+                            simu_cfg.update(lb_scheme.get_dictionary())
+                            param.update(simu_cfg['parameters'])
+                            simu_cfg['parameters'] = param
+                            if codegen.v_model != 'auto':
+                                simu_cfg['generator'] = codegen.v_model
+                            simu_cfg['codegen_option'] = {'directory': tmp_dir.name}
+                            simu_cfg['space_step'] = float(space_step.v_model)
 
-                        keys = list(simu_cfg['parameters'].keys())
-                        str_keys = [str(k) for k in simu_cfg['parameters'].keys()]
-                        for ik, k in enumerate(design_space.keys()):
-                            if isinstance(k, tuple):
-                                for kk in k:
-                                    if kk in str_keys:
-                                        simu_cfg['parameters'].pop(keys[str_keys.index(kk)])
-                            else:
-                                if k in str_keys:
-                                    simu_cfg['parameters'].pop(keys[str_keys.index(k)])
+                            bound_cfg = {}
+                            bound_tc = test_case.get_boundary()
+                            bound_sc = lb_scheme.get_boundary()
+                            for key, val in bound_tc.items():
+                                bound_cfg[key] = bound_sc[val]
 
-                        pylbm.Simulation(simu_cfg, initialize=False)
+                            simu_cfg.pop('dim')
+                            simu_cfg['boundary_conditions'] = bound_cfg
 
-                        args = []
-                        for s in sampling:
-                            design_sample = {}
+                            keys = list(simu_cfg['parameters'].keys())
+                            str_keys = [str(k) for k in simu_cfg['parameters'].keys()]
                             for ik, k in enumerate(design_space.keys()):
                                 if isinstance(k, tuple):
                                     for kk in k:
-                                        design_sample[kk] = s[ik]
+                                        if kk in str_keys:
+                                            simu_cfg['parameters'].pop(keys[str_keys.index(kk)])
                                 else:
-                                    if k == 'lambda':
-                                        design_sample['lambda_'] = s[ik]
-                                    design_sample[k] = s[ik]
-                            args.append((simu_cfg, design_sample, test_case.duration))
+                                    if k in str_keys:
+                                        simu_cfg['parameters'].pop(keys[str_keys.index(k)])
 
-                        output[:] = run_parametric_study(args)
+                            alert.children = ['Prepare the simulation...']
 
-                        dimensions = [dict(values=sampling[:, ik], label=f'{k}') for ik, k in enumerate(design_space.keys())]
-                        dimensions.append(dict(values=output, label='stability'))
+                            pylbm.Simulation(simu_cfg, initialize=False)
 
-                        fig = go.FigureWidget(
-                            data=go.Parcoords(
-                                line=dict(color = output),
-                                dimensions = dimensions,
-                            ),
-                        )
+                            args = []
+                            for s in sampling:
+                                design_sample = {}
+                                for ik, k in enumerate(design_space.keys()):
+                                    if isinstance(k, tuple):
+                                        for kk in k:
+                                            design_sample[kk] = s[ik]
+                                    else:
+                                        if k == 'lambda':
+                                            design_sample['lambda_'] = s[ik]
+                                        design_sample[k] = s[ik]
+                                args.append((simu_cfg, design_sample, test_case.duration))
 
-                        fig.update_layout(
-                            autosize=False,
-                            width=800,
-                            height=600,)
+                            alert.children = ['Run simulations on the sampling...']
 
-                        plotly_plot.children = [fig]
-                        stop_simulation(None)
+                            # async def run_parametric_study():
+                            def run_parametric_study():
+                                with out:
+                                    from pathos.multiprocessing import ProcessingPool
+                                    pool = pp.ProcessPool(nodes=4)
+                                    output[:] = pool.map(run_simulation, args)
+                                    dimensions = [dict(values=sampling[:, ik], label=f'{k}') for ik, k in enumerate(design_space.keys())]
+                                    dimensions.append(dict(values=output, label='stability'))
+
+                                    fig = go.FigureWidget(
+                                            data=go.Parcoords(
+                                            line=dict(color = output),
+                                            dimensions = dimensions,
+                                        ),
+                                    )
+
+                                    fig.update_layout(
+                                        autosize=False,
+                                        width=800,
+                                        height=600,)
+
+                                    plotly_plot.children = [v.Row(children=[fig], align='center', justify='center')]
+                                    stop_simulation(None)
+                            # asyncio.ensure_future(run_parametric_study())
+                            run_parametric_study()
+
+                            # output[:] = run_parametric_study(args)
+
+                            # dimensions = [dict(values=sampling[:, ik], label=f'{k}') for ik, k in enumerate(design_space.keys())]
+                            # dimensions.append(dict(values=output, label='stability'))
+
+                            # fig = go.FigureWidget(
+                            #     data=go.Parcoords(
+                            #         line=dict(color = output),
+                            #         dimensions = dimensions,
+                            #     ),
+                            # )
+
+                            # fig.update_layout(
+                            #     autosize=False,
+                            #     width=800,
+                            #     height=600,)
+
+                            # plotly_plot.children = [fig]
+                        # stop_simulation(None)
 
                     else:
                         stop_simulation(None)
